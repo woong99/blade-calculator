807275263938a175e15dc5dd4d95e9c14fcdefe2
Woong99 <dndry1072@naver.com>
2023-05-02 09:12:46 +0900

    [feat] : 2023-05-02 main, calculator 컨트롤러 추가

--- src/main/java/com/example/bladecalculator/controller/CalculatorController.java
+++ src/main/java/com/example/bladecalculator/controller/CalculatorController.java
@@ -2,28 +2,19 @@ package com.example.bladecalculator.controller;
 
 import com.example.bladecalculator.auth.annotation.AuthUser;
 import com.example.bladecalculator.entity.User;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/calculator")
 @RequiredArgsConstructor
 @Slf4j
-public class MainController {
+public class CalculatorController {
 
-    @RequestMapping("/")
-    public String main() throws IOException {
-        return "/views/user/main";
-    }
-
-
-    @RequestMapping("/login")
-    public String login(@AuthUser User user) {
-        if (user != null) {
-            return "redirect:/";
-        }
-        return "/views/login";
+    @RequestMapping("/hp")
+    public String hp(@AuthUser User user) {
+        return "/views/user/calculator/hp";
     }
 }
