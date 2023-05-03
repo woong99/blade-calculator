2180f84f522ad14b2a6b0e62cc2bcdaa8e2822aa
Woong99 <dndry1072@naver.com>
2023-05-03 14:24:53 +0900

    [feat] : 2023-05-03 bignumber.js 추가

--- src/main/java/com/example/bladecalculator/entity/User.java
+++ src/main/java/com/example/bladecalculator/entity/User.java
@@ -4,17 +4,23 @@ import com.example.bladecalculator.common.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import lombok.ToString.Exclude;
 
 @Entity
 @AllArgsConstructor
@@ -39,5 +45,10 @@ public class User extends BaseEntity implements Serializable {
     private Authority authority;
 
     private LocalDateTime lastLoginAt;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @Setter
+    @Exclude
+    private List<UserStat> userStats = new ArrayList<>();
 
 }
